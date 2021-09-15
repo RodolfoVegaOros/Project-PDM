baa76cddd88e3043dbd2c51b6fb42de81cf44c77
Alumno <ulsa@D110.local>
2021-09-14 17:55:50 -0700

    first commit

--- Interaccion Imagen/ViewController.swift
+++ Interaccion Imagen/ViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTitulo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func doTaoanimalito(_ sender: Any) {
+        lblTitulo.text = "cat"
     }
+
 
 
 }
